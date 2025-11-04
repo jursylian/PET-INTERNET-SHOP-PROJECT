c3017ddf181c458d728181dca6b023ba5da9a350
Juliya <jursylian@gmail.com>
2025-11-04 23:33:08 +0200

    added page not found for uncorrect URL

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -10,39 +10,42 @@ function Header() {
 
   return (
     <header className={styles.header}>
+      {/* Левая часть */}
       <div className={styles.left}>
         <Link to="/" className={styles.logo}>
           <img src={logo} alt="Логотип" />
         </Link>
-
-        <nav className={styles.nav}>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Main Page
-          </NavLink>
-          <NavLink
-            to="/categories"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Categories
-          </NavLink>
-          <NavLink
-            to="/products"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            All products
-          </NavLink>
-          <NavLink
-            to="/discounts"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            All sales
-          </NavLink>
-        </nav>
       </div>
 
+      {/* Центр */}
+      <nav className={styles.nav}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.active : "")}
+        >
+          Main Page
+        </NavLink>
+        <NavLink
+          to="/categories"
+          className={({ isActive }) => (isActive ? styles.active : "")}
+        >
+          Categories
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive }) => (isActive ? styles.active : "")}
+        >
+          All products
+        </NavLink>
+        <NavLink
+          to="/discounts"
+          className={({ isActive }) => (isActive ? styles.active : "")}
+        >
+          All sales
+        </NavLink>
+      </nav>
+
+      {/* Правая часть */}
       <div className={styles.right}>
         <Link to="/cart" className={styles.cart}>
           <img src={cartIcon} alt="Корзина" />
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./NotFound.module.css";
+import dog404 from "../../assets/404.png";
 
-const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
-};
+function NotFound() {
+  return (
+    <section className={styles.notFound}>
+      <div className={styles.errorBlock}>
+        <div className={styles.errorCode}>
+          <img src={dog404} alt="Dog" className={styles.dog} />
+        </div>
 
-export default NotFoundPage;
+        <h2 className={styles.title}>Page Not Found</h2>
+        <p className={styles.text}>
+          We're sorry, the page you requested could not be found.
+          <br /> Please go back to the homepage.
+        </p>
+
+        <Link to="/" className={styles.button}>
+          Go Home
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default NotFound;
