4bec4dcbcc3577f060a77a6c5a5f5fe28e6f1861
Yuliia Ursulian <jursylian.com>
2026-03-30 19:57:11 +0300

    fix api routes

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://petshop-backend.onrender.com",
+  baseURL: "https://petshop-backend.onrender.com",
 });
 
 export const getCategories = () => api.get("/categories/all");
 
-export const getSaleProducts = () => api.get("/products/sale");
+export const getSaleProducts = () => api.get("/products/all");
 
 export const sendSaleForm = (data) => api.post("/sale/send", data);
 
