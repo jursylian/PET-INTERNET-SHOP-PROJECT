3570e73e803f2d528f59fd83728c0cfb087387f4
Yuliia Ursulian <jursylian.com>
2026-03-30 20:12:35 +0300

    use new render backend url

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://petshop-backend.onrender.com",
+  baseURL: "https://pet-shop-backend-ljeu.onrender.com",
 });
 
 export const getCategories = () => api.get("/categories/all");
-
 export const getSaleProducts = () => api.get("/products/all");
-
 export const sendSaleForm = (data) => api.post("/sale/send", data);
 
 export default api;
