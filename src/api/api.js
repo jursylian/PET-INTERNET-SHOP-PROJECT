9b7ea6db2d31745359910f131e5913bb574dceba
Yuliia Ursulian <jursylian.com>
2026-03-30 19:40:23 +0300

    connect backend to render

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://petshop-backend.onrender.com",
 });
 
 export const getCategories = () => api.get("/categories/all");
